1c9abae7252e8adf1fdcb91efe0eba4a2f9e53d4
150202-Pratham <140713281+150202-Pratham@users.noreply.github.com>
2025-07-25 12:42:15 +0530

    Added Some New Entities to the Project

---
+++ src/main/java/ecommerce/ecommerce_backend/Entities/Address.java
@@ -0,0 +1,41 @@
+package ecommerce.ecommerce_backend.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+
+
+    private String name ;
+
+    private String locality ;
+
+    private String address ;
+
+    private String city ;
+
+    private String state ;
+
+    private String pinCode ;
+
+    private String mobile ;
+
+
+
+
+
+}
